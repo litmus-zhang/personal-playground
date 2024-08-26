f773b66cc60b5c492a67c2ec3053a972047f18c8
litmus-zhang <abdulsalamlukmon9@gmail.com>
2024-08-26 11:54:42 +0100

    updated the word counter cli app to count bytes

--- devtools/cli_apps/counter/main.go
+++ devtools/cli_apps/counter/main.go
@@ -1,4 +1,4 @@
-package main
+package counter
 
 import (
 	"bufio"
@@ -10,15 +10,20 @@ import (
 
 func main() {
 	lines := flag.Bool("l", false, "count lines")
+	bytes := flag.Bool("b", false, "count bytes")
 	flag.Parse()
-	fmt.Println(count(os.Stdin, *lines))
+	fmt.Println(count(os.Stdin, *lines, *bytes))
 }
 
-func count(r io.Reader, countLines bool) int {
+func count(r io.Reader, countLines bool, bytes bool) int {
 	scanner := bufio.NewScanner(r)
 
 	if !countLines {
 		scanner.Split(bufio.ScanWords)
+	}
+
+	if bytes {
+		scanner.Split(bufio.ScanBytes)
 	}
 
 	wc := 0
--- devtools/cli_apps/counter/main_test.go
+++ devtools/cli_apps/counter/main_test.go
@@ -1,4 +1,4 @@
-package main
+package counter
 
 import (
 	"bytes"
@@ -6,20 +6,30 @@ import (
 )
 
 func TestCountWords(t *testing.T) {
-	b := bytes.NewBufferString("hello world of songs")
+	b := bytes.NewBufferString("hello world of songs\n")
 	exp := 4
 
-	res := count(b, false)
+	res := count(b, false, false)
 	if res != exp {
 		t.Errorf("Expected %v, got %v", exp, res)
 	}
 }
 
 func TestCountLines(t *testing.T) {
-	b := bytes.NewBufferString("hello\nworld\nof\nsongs")
-	exp := 4
+	b := bytes.NewBufferString("word1 word2 word3 \n line2 \n line3 word1")
+	exp := 3
 
-	res := count(b, true)
+	res := count(b, true, false)
+	if res != exp {
+		t.Errorf("Expected %v, got %v", exp, res)
+	}
+}
+
+func TestBytes(t *testing.T) {
+	b := bytes.NewBufferString("hello world of songs")
+	exp := 20
+
+	res := count(b, false, true)
 	if res != exp {
 		t.Errorf("Expected %v, got %v", exp, res)
 	}
