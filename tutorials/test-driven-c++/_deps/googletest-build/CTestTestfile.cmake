a3cc9bb6d32eaaa11c6d956a6f0093810cb1172b
litmus-zhang <abdulsalamlukmon9@gmail.com>
2025-03-16 09:44:12 +0000

    getting started with c++

---
+++ tutorials/test-driven-c++/_deps/googletest-build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /workspace/personal-playground/tutorials/test-driven-c++/_deps/googletest-src
+# Build directory: /workspace/personal-playground/tutorials/test-driven-c++/_deps/googletest-build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("googlemock")
